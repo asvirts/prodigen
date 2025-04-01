6a0d85c13205185c1c07b6213ed852bd3e3760ce
Andrew Virts <asvirts@outlook.com>
2025-04-01 10:06:38 -0500

    auth page w-full

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -91,7 +91,7 @@ export default function AuthPage() {
   }
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
+    <div className="flex items-center w-full justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
       <Card className="w-full max-w-sm">
         <CardHeader>
           <CardTitle className="text-2xl">Login / Sign Up</CardTitle>
