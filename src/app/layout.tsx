2e945dda97361dbe1e3805fb6e373adbc56fe468
Andrew Virts <asvirts@outlook.com>
2025-04-01 10:07:59 -0500

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google" // Add Inter
 import { Toaster } from "sonner" // Correct the import path for Toaster
 import { Providers } from "./providers" // Import our custom Providers component
 import "./globals.css"
-
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" }) // Setup Inter
 
 export const metadata: Metadata = {
@@ -20,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.variable} font-sans antialiased`}>
+        <Analytics />
         <Providers>
           <div className="flex justify-center">{children}</div>
           <Toaster richColors position="top-right" />
